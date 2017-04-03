e59314ff8fac3aead43f6559e0712e948c5c15f1
albokasoft <info@albokasoft.com>
2017-04-03 12:46:09 +0200

    Create backup.sh

---
+++ files/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO_BASE=/var/local/svn
+TARGET=/opt/backup/
+SVNADMIN=/usr/bin/svnadmin
+GZIP=/bin/gzip
+
+cd "$REPO_BASE"
+for f in *; do
+        FILE="$TARGET$f.dump.gz"
+        echo "Dump: $f => $FILE"
+    test -d "$f"  &&  $SVNADMIN dump "$f" | "$GZIP" -9 > "$FILE"
+done
